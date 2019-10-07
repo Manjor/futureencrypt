bb8ebad36b73450e8745d63d0daa91965285159c
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2019-10-07 16:25:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist/"));
-app.get(/.*/,function(req,res){
-    res.sendfile(__dirname + "/dist/index.html")
+const express = require('express')
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(express.static(__dirname + "/dist/"))
+app.get(/.*/, function(req,res){
+    res.sendFile(__dirname + "/dist/index.html");
 })
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+
+app.listen(port)
